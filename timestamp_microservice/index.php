6643d9fc203b106e896c4f1834c7e138ff82cc73
odekyc <odekyc@gmail.com>
2016-10-09 21:33:34 +0000

    hi

--- timestamp_microservice/index.php
+++ timestamp_microservice/index.php
@@ -7,7 +7,12 @@
   echo "<center><h1>Time Stamp Microservice</h1></center>";
   echo "<center><h3>If you append a date string after the URL http://odetimestamp.herokuapp.com/, it would return an object containing both the Unix Timestamp and the Natural Language Date.</h3></center>";
   echo "<center><h3>The date string you append after the URL can be either a Unix Timestamp number or a date separated by % in the format (e.g. January%1%2016).</h3><center>";
-  echo "<center><h5 style="color:red; background-color:pink;">hihi</h5></center>"; 
+  echo "<center><h4 style='color:red; background-color:pink; display: inline'>example: http://odetimestamp.herokuapp.com/December%15%2015 </h4></center>";
+  echo "<center><h4 style='color:red; background-color:pink; display: inline'>example: http://odetimestamp.herokuapp.com/1450137600 </h4></center>";
+  echo "<center><h4 style='color:red; background-color:pink; display: inline'>output: { 'unix': 1450137600, 'natural': 'December 15, 2015' }</h4></center>";
+  echo "<center><h3>If you append a date string without following the format above,it would return 'Bad Request'</h3></center>";
+  echo "<center><h4 style='color:red; background-color:pink; display: inline'>example: http://odetimestamp.herokuapp.com/December%2015,%2015 </h4></center>";
+  echo "<center><h4 style='color:red; background-color:pink; display: inline'>output: 'Bad Request'</h4></center>";
  $url = $_SERVER['REQUEST_URI'];
  
  echo $url;
