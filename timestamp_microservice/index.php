0d1d7bffddd0cf0d0cd834260bc360de7ca2560c
odekyc <odekyc@gmail.com>
2016-10-10 16:58:21 +0000

    hi

--- timestamp_microservice/index.php
+++ timestamp_microservice/index.php
@@ -5,11 +5,16 @@
 // then click the URL that is emitted to the Output tab of the console
   $url = $_SERVER['REQUEST_URI'];
   $months = array("January", "February", "March", "April", "May", "June", "July","August", "September", "October", "November", "December");
-   
+  $months_31days= array("January", "March", "May", "July", "August", "October", "December");
+  $date_valid=true;
+  
   if($url==="/"){
+  
+    
+    
     echo "<center><h1>Time Stamp Microservice</h1></center>";
-    echo "<center><h3>If you append a date string after the URL http://odetimestamp.herokuapp.com/, it would return an object containing both the Unix Timestamp and the Natural Language Date.</h3></center>";
-    echo "<center><h3>The date string you append after the URL can be either a Unix Timestamp number or a date separated by % in the format (e.g. January%1%2016).</h3><center>";
+    echo "<center><h3>If you append a date string after the URL http://odetimestamp.herokuapp.com/, it would return an object containing both the Unix Timestamp as of 12AM ( 0 hour 0 minite 0 second) on that date and the Natural Language Date.</h3></center>";
+    echo "<center><h3>The date string you append after the URL must be either a Unix Timestamp number or a date separated by % in the format (e.g. January%1%2016).</h3><center>";
     echo "<center><h4 style='color:red; background-color:pink; display: inline'>example: http://odetimestamp.herokuapp.com/December%15%2015 </h4></center><br>";
     echo "<center><h4 style='color:red; background-color:pink; display: inline'>example: http://odetimestamp.herokuapp.com/1450137600 </h4></center><br>";
     echo "<center><h4 style='color:red; background-color:pink; display: inline'>output: { 'unix': 1450137600, 'natural': 'December 15, 2015' }</h4></center>";
@@ -24,6 +29,7 @@
       
       if(strlen($url)<10){
         echo "Bad Request";
+        $date_valid=false;
       }
       else{
         $result= date('F d, Y', $url);
@@ -35,34 +41,74 @@
     else{
       $mypos=strpos($url, "%");
       if($mypos){
-        echo "good request string";
         $date_arr=explode( "%",$url);
         if(sizeof($date_arr)<3){
           echo "Bad Request";
+          $date_valid=false;
         }
         elseif($date_arr[2]===""){
           echo "Bad Request";
+          $date_valid=false;
         }
         else{
-          print_r($date_arr);
-          
+          $nyear= $date_arr[2];
           $nmonth = array_search($date_arr[0], $months);
+          $ndate = $date_arr[1];
           
-          if($nmonth or $nmonth===0){
-            $nmonth+=1;
-            echo $nmonth;
-          }
-          else{
-            echo "Bad Request";
-          }
+          if(strlen($nyear)===4){
+            
+            if($nmonth or $nmonth===0){
+              $nmonth+=1;
+              if($ndate!==""){
+                
+                $is_31days_month= array_search($date_arr[0], $months_31days);
+                if($nmonth===2){
+                  if(($date_arr[2]%400===0) or ($date_arr[2]%4===0 and $date_arr[2]%100!==0)){
+                    if($ndate>0 and $ndate<30){
+                    }
+                    else{
+                      echo "Bad Request";
+                      $date_valid=false;
+                    }
+                  }
+                  else{
+                    if($ndate>0 and $ndate<29){
+                    }
+                    else{
+                      echo "Bad Request";
+                      $date_valid=false;
+                    }
+                  }
+                }
+                
+              }
+              else{
+                 echo "Bad Request";
+                 $date_valid=false;
+              }
+            }
+            else{
+              echo "Bad Request";
+              $date_valid=false;
+            }
+         }
+         else{
+           echo "Bad Request";
+           $date_valid=false;
+         }
         }
         
       }
       else{
         echo 'Bad Request';
+        $date_valid=false;
       }
     }
-      
+      if($date_valid){
+         $unix = mktime(0, 0, 0, $nmonth, $ndate, $nyear);
+         $naturaldate=date('F d, Y', $unix);
+         echo "{ 'unix' : {$unix} ,  natural' : {$naturaldate} }"; 
+      }
   }
  
  
