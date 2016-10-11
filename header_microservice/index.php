39de7a5ba5b6ab732e8963bf7c64664edcd62e6e
odekyc <odekyc@gmail.com>
2016-10-11 02:04:45 +0000

    hi

--- header_microservice/index.php
+++ header_microservice/index.php
@@ -1,11 +1,12 @@
 <html><body>
 <?php
+
+    echo "<h3>ip address: {$_SERVER['REMOTE_ADDR']}</h3><br>";
     $lang =explode(",", $_SERVER['HTTP_ACCEPT_LANGUAGE']);
-    echo "ip address: { $_SERVER['REMOTE_ADDR'] }";
-    echo "language: {$lang[0] }";
+    echo "<h3>language: {$lang[0] }</h3><br >";
     $software=explode("(", $_SERVER['HTTP_USER_AGENT'])[1];
     $software=explode(")", $software)[0];
-    echo "software: {$software}";
+    echo "<h3>software: {$software}</h3><br>";
 ?>
 </body>
 </html>
