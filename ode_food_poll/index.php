c620cd0c12766ee08477a0075e75db35ed27a7e4
odekyc <odekyc@gmail.com>
2016-10-25 05:41:42 +0000

    hi

--- ode_food_poll/index.php
+++ ode_food_poll/index.php
@@ -5,24 +5,7 @@
      <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
    </head>
 <body>
-
-<?php
-     
-     include 'EpiCurl.php';
-     include 'EpiOAuth.php';
-     include 'EpiTwitter.php';
-     include 'keys.php';
-    
-     session_start();
-     
-   if(empty($_SESSION['TwitterUsername']))
-   {
-       header("Location: NotLoggedIn.php");
-   }
-    
-?>
-
- <div id='upper-div'><h1 id='upper-div-title'>Ode-Food-Poll</h1><div id='home-div-in' class='block'><span class='block-span'>Home</span></div><div id='mypolls' class='block'><span id='mypolls-span' class='block-span'><center>My Polls</center></span><</div><div id='newpolls' class='block'><span id='newpoll-span' class='block-span'>New Poll</span></div></div>
+<div id='upper-div'><h1 id='upper-div-title'>Ode-Food-Poll</h1><div id='home-div-in' class='block'><span class='block-span'>Home</span></div><div id='mypolls' class='block'><span id='mypolls-span' class='block-span'><center>My Polls</center></span><</div><div id='newpolls' class='block'><span id='newpoll-span' class='block-span'>New Poll</span><</div></div>
      <div id='main-div'><div id='main-div-title'><span id='main-title-span'>Ode's Food Poll</span><span id='main-descrpt-span'>Below are polls hosted by Ode<br>Select a poll to see the results and vote, or sign-in to make a new poll.</span></div>
      <div id='central-div'>
        <div id='div-1' class='ingre-type'><span class='ingre-name'>Meat</span></div>
@@ -117,5 +100,22 @@
      </div>
       <h5>This "Ode Food Poll" app is built by <a href="https://github.com/odekyc">@Ode</a> of freecodecamp<br><br> following the instructions of <a href="https://www.freecodecamp.com/challenges/build-a-voting-app">"Basejump: Build a Voting App | Free Code Camp"</a><br><br>Github repository: <a href="https://github.com/odekyc">https://github.com/odekyc</a><br><br>Code Pen: <a href="http://codepen.io/odekyc/">http://codepen.io/odekyc/</a></h5>
      
+<?php
+     
+     include 'EpiCurl.php';
+     include 'EpiOAuth.php';
+     include 'EpiTwitter.php';
+     include 'keys.php';
+    
+     session_start();
+     
+   if(empty($_SESSION['TwitterUsername']))
+   {
+    //   header("Location: NotLoggedIn.php");
+   }
+    
+?>
+
+ 
 </body>
 </html>
