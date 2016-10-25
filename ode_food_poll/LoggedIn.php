c9ccdfbc16ba232284763c5d6702d8324e014ba3
odekyc <odekyc@gmail.com>
2016-10-25 21:08:16 +0000

    hi

--- ode_food_poll/LoggedIn.php
+++ ode_food_poll/LoggedIn.php
@@ -107,3 +107,11 @@
      </div>
       <h5>This "Ode Food Poll" app is built by <a href="https://github.com/odekyc">@Ode</a> of freecodecamp<br><br> following the instructions of <a href="https://www.freecodecamp.com/challenges/build-a-voting-app">"Basejump: Build a Voting App | Free Code Camp"</a><br><br>Github repository: <a href="https://github.com/odekyc">https://github.com/odekyc</a><br><br>Code Pen: <a href="http://codepen.io/odekyc/">http://codepen.io/odekyc/</a></h5>
      
+     
+     
+
+<?php 
+     echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>';
+     echo '<script>$("#home-div-in").click(function(){alert("hi");});</script>';
+     
+?>
