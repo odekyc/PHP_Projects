8515d0a11e7af1969dcb0ecce28c8cdd0ba18da4
odekyc <odekyc@gmail.com>
2016-10-26 16:10:15 +0000

    hi

--- ode_food_poll/LoggedIn.php
+++ ode_food_poll/LoggedIn.php
@@ -12,7 +12,7 @@
   
 ?>
 
-<div id='upper-div'><h1 id='upper-div-title'>Ode-Food-Poll</h1><div id='home-div-in' class='block'><span class='block-span'>Home</span></div><div id='mypolls' class='block'><span id='mypolls-span' class='block-span'><center>My Polls</center></span><</div><div id='newpolls' class='block'><span id='newpoll-span' class='block-span'>New Poll</span><</div></div>
+<div id='upper-div'><h1 id='upper-div-title'>Ode-Food-Poll</h1><div id='home-div-in' class='block'><span class='block-span'>Home</span></div><div id='mypolls' class='block'><span id='mypolls-span' class='block-span'><center>My Polls</center></span><</div><div id='newpoll' class='block'><span id='newpoll-span' class='block-span'>New Poll</span><</div></div>
      <div id='main-div'><div id='main-div-title'><span id='main-title-span'>Ode's Food Poll</span><span id='main-descrpt-span'>Below are polls hosted by Ode<br>Select a poll to see the results and vote, or sign-in to make a new poll.</span></div>
      <div id='central-div'>
        <div id='div-1' class='ingre-type'><span class='ingre-name'>Meat</span></div>
@@ -107,5 +107,14 @@
      </div>
       <h5>This "Ode Food Poll" app is built by <a href="https://github.com/odekyc">@Ode</a> of freecodecamp<br><br> following the instructions of <a href="https://www.freecodecamp.com/challenges/build-a-voting-app">"Basejump: Build a Voting App | Free Code Camp"</a><br><br>Github repository: <a href="https://github.com/odekyc">https://github.com/odekyc</a><br><br>Code Pen: <a href="http://codepen.io/odekyc/">http://codepen.io/odekyc/</a></h5>
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script type="text/javascript">$("#home-div-in").click(function(){ window.location.href = "MyPolls.php"; });</script>
+    <script type="text/javascript">
+    
+    $("#mypolls").click(function(){
+        window.location.href = "MyPolls.php";
+        $('#home-div-in').css("background-color", "#99ceff");
+        $('#mypolls').css("background-color", "#ace600");
+    });
+
+    $("#newpoll").click(function(){ window.location.href = "NewPoll.php"; });
+    </script>
      
--- ode_food_poll/MyPolls.php
+++ ode_food_poll/MyPolls.php
@@ -1,5 +1,31 @@
 <?php
+     
+     include 'EpiCurl.php';
+     include 'EpiOAuth.php';
+     include 'EpiTwitter.php';
+     include 'keys.php';
+     
+     
+     echo '<a id="twitterbtnsignout" href="TwitterLogout.php">Logout</a>';
 
-    echo "hi Jason";
+?>
+
+<link rel='stylesheet' type='text/css' href='stylesheet.css' />
+
+<div id='upper-div'><h1 id='upper-div-title'>Ode-Food-Poll</h1><div id='home-div-in' class='block'><span class='block-span'>Home</span></div><div id='mypolls' class='block'><span id='mypolls-span' class='block-span'><center>My Polls</center></span><</div><div id='newpoll' class='block'><span id='newpoll-span' class='block-span'>New Poll</span><</div></div>
+<div id='mypolls-main-div'></div>
+<h5 id="underscript-mypoll">This "Ode Food Poll" app is built by <a href="https://github.com/odekyc">@Ode</a> of freecodecamp<br><br> following the instructions of <a href="https://www.freecodecamp.com/challenges/build-a-voting-app">"Basejump: Build a Voting App | Free Code Camp"</a><br><br>Github repository: <a href="https://github.com/odekyc">https://github.com/odekyc</a><br><br>Code Pen: <a href="http://codepen.io/odekyc/">http://codepen.io/odekyc/</a></h5>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script type="text/javascript">
+    $('html').height("120%");
+    $('#home-div-in').css("background-color", "#99ceff");
+    $('#home-div-in').hover(function(){   $('#home-div-in').css("background-color", "#ace600");} , function(){ $('#home-div-in').css("background-color", "#99ceff"); });
+    $('#mypolls').css("background-color", "#ace600");
+    
+</script>
+
+<?php
+
+  
 
 ?>
