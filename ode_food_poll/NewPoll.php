b32e16e274e5705be98f3b8ba1d5e919b309ad07
odekyc <odekyc@gmail.com>
2016-10-28 06:17:40 +0000

    hi

--- ode_food_poll/NewPoll.php
+++ ode_food_poll/NewPoll.php
@@ -13,7 +13,7 @@
 <link rel='stylesheet' type='text/css' href='stylesheet.css' />
 
 <div id='upper-div'><h1 id='upper-div-title'>Ode-Food-Poll</h1><div id='home-div-in' class='block'><span class='block-span'>Home</span></div><div id='mypolls' class='block'><span id='mypolls-span' class='block-span'><center>My Polls</center></span><</div><div id='newpoll' class='block'><span id='newpoll-span' class='block-span'>New Poll</span><</div></div>
-<div id='mypolls-main-div'> <div id='main-div-title'></div></div>
+<div id='mypolls-main-div'><div id='main-div-title'><span id='newpoll-main-title-span'>Make a New Poll:</span></div></div>
 <h5 id="underscript-newpoll">This "Ode Food Poll" app is built by <a href="https://github.com/odekyc">@Ode</a> of freecodecamp<br><br> following the instructions of <a href="https://www.freecodecamp.com/challenges/build-a-voting-app">"Basejump: Build a Voting App | Free Code Camp"</a><br><br>Github repository: <a href="https://github.com/odekyc">https://github.com/odekyc</a><br><br>Code Pen: <a href="http://codepen.io/odekyc/">http://codepen.io/odekyc/</a></h5>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script type="text/javascript">
