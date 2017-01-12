c321f103189281b909cd227498b0f8345de8fe85
odekyc <odekyc@gmail.com>
2017-01-12 23:43:04 +0000

    hi

--- ode_food_poll/VotingPollOut.php
+++ ode_food_poll/VotingPollOut.php
@@ -1,7 +1,7 @@
 <head>
      <link rel='stylesheet' type='text/css' href='stylesheet.css?<?php echo time(); ?>' />
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/4.4.1/d3.min.js"></script>
 </head>
 
 <?php
@@ -51,7 +51,23 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
 <div id='upper-div'><h1 id='upper-div-title'>Ode-Food-Poll</h1><div id='home-div-out' class='block'><span class='block-span'>Home</span></div></div> 
- <div id='voting-poll-div'><div id="chart">hihiih</div> </div>
+ <div id='voting-poll-div'><div id="chart">hihiih</div> <?php
+             $sql = "SELECT * FROM food_list";
+            $result = $conn->query($sql);
+            $row_count=0;
+            if ($result->num_rows > 0) {
+                // output data of each row
+               
+               
+            } else {
+                echo "0 results";
+            }
+            
+           
+            $conn->close();
+            
+    ?>
+     </div>
 
 <h5 id="underscript-newpoll">This "Ode Food Poll" app is built by <a href="https://github.com/odekyc">@Ode</a> of freecodecamp<br><br> following the instructions of <a href="https://www.freecodecamp.com/challenges/build-a-voting-app">"Basejump: Build a Voting App | Free Code Camp"</a><br><br>Github repository: <a href="https://github.com/odekyc">https://github.com/odekyc</a><br><br>Code Pen: <a href="http://codepen.io/odekyc/">http://codepen.io/odekyc/</a></h5>
  
