f577a5aa148c0403d201823b10c28a551c6da67e
odekyc <odekyc@gmail.com>
2017-01-18 21:16:14 +0000

    hi

--- ode_food_poll/VotingPollIn.php
+++ ode_food_poll/VotingPollIn.php
@@ -94,7 +94,7 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
 <div id='upper-div'><h1 id='upper-div-title'>Ode-Food-Poll</h1><div id='home-div-in' class='block'><span class='block-span'>Home</span></div><div id='mypolls' class='block'><span id='mypolls-span' class='block-span'><center>My Polls</center></span><</div><div id='newpoll' class='block'><span id='newpoll-span' class='block-span'>New Poll</span><</div></div>  
-     <div id='voting-poll-div'><div id="foodname-div"><span id="foodname-span"></span><span id="serving-std-span"></span><span id="serving_sz">Daily Serving Sizes</span><span id="actual_serving_ct">(Actual Daily Servings Vote Counts)</span><span id="idliketovote">I'd Like to Vote For(Daily Serving Size):</span>       <form action="" id="voteform">
+     <div id='voting-poll-div'><div id="foodname-div"><span id="foodname-span"></span><span id="serving-std-span"></span><span id="serving_sz">Daily Serving Sizes</span><span id="actual_serving_ct">(Actual Daily Servings Vote Counts)</span><span id="idliketovote">I'd Like to Vote For(Daily Serving Size):</span> <a id="tweet-but" href="https://twitter.com/share/tweet?text=Ode's%20Food%20Poll%20@" data-size="large" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script> <form action="" id="voteform">
            <select id="voteselect">
               <option id="firstvoteop" value="">first</option>
               <option id="secondvoteop" value="">second</option>
@@ -104,7 +104,7 @@
          
           <input id="votesubmit" type="submit" value="Submit">
         </form>
-<a id="tweet-but" href="https://twitter.com/share/tweet?text=Ode's%20Food%20Poll%20@" data-size="large" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script></div><div id="chart"></div></div>
+</div><div id="chart"></div></div>
 
 <h5 id="underscript-newpoll">This "Ode Food Poll" app is built by <a href="https://github.com/odekyc">@Ode</a> of freecodecamp<br><br> following the instructions of <a href="https://www.freecodecamp.com/challenges/build-a-voting-app">"Basejump: Build a Voting App | Free Code Camp"</a><br><br>Github repository: <a href="https://github.com/odekyc">https://github.com/odekyc</a><br><br>Code Pen: <a href="http://codepen.io/odekyc/">http://codepen.io/odekyc/</a></h5>
 <script type='text/javascript'>
