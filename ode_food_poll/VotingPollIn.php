7cff124f3ecc30b15c4058d70b81a0c31825872c
odekyc <odekyc@gmail.com>
2017-01-18 04:49:34 +0000

    hi

--- ode_food_poll/VotingPollIn.php
+++ ode_food_poll/VotingPollIn.php
@@ -94,7 +94,7 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
 <div id='upper-div'><h1 id='upper-div-title'>Ode-Food-Poll</h1><div id='home-div-in' class='block'><span class='block-span'>Home</span></div><div id='mypolls' class='block'><span id='mypolls-span' class='block-span'><center>My Polls</center></span><</div><div id='newpoll' class='block'><span id='newpoll-span' class='block-span'>New Poll</span><</div></div>  
-     <div id='voting-poll-div'><div id="foodname-div"><span id="foodname-span"></span><span id="serving-std-span"></span><span id="serving_sz">Daily Serving Sizes</span><span id="actual_serving_ct">(Actual Daily Servings Vote Counts)</span></div><div id="chart"></div></div>
+     <div id='voting-poll-div'><div id="foodname-div"><span id="foodname-span"></span><span id="serving-std-span"></span><span id="serving_sz">Daily Serving Sizes</span><span id="actual_serving_ct">(Actual Daily Servings Vote Counts)</span><span id="idliketovote">I'd Like to Vote For:</span></span></div><div id="chart"></div></div>
 
 <h5 id="underscript-newpoll">This "Ode Food Poll" app is built by <a href="https://github.com/odekyc">@Ode</a> of freecodecamp<br><br> following the instructions of <a href="https://www.freecodecamp.com/challenges/build-a-voting-app">"Basejump: Build a Voting App | Free Code Camp"</a><br><br>Github repository: <a href="https://github.com/odekyc">https://github.com/odekyc</a><br><br>Code Pen: <a href="http://codepen.io/odekyc/">http://codepen.io/odekyc/</a></h5>
 <script type='text/javascript'>
@@ -196,12 +196,16 @@
         var serving_sz_height=(foodname_lines*85)+125;
         
         var actual_servingct_height=(foodname_lines*85)+175;
+        
+        var idliketovote_height=(foodname_lines*85)+265;
     
         $("#foodname-span").html(revised_foodname);
         
         $("#serving_sz").css("top", serving_sz_height);
         
          $("#actual_serving_ct").css("top", actual_servingct_height);
+         
+         $("#idliketovote").css("top", idliketovote_height);
         
         $("#serving-std-span").text(serving_std+"(s)");
         
