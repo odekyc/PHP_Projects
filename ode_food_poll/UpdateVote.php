3ce27836b1bbe91a2039ccf3fe7fa18bf1835e25
odekyc <odekyc@gmail.com>
2017-01-20 09:49:15 +0000

    hi

--- ode_food_poll/UpdateVote.php
+++ ode_food_poll/UpdateVote.php
@@ -9,8 +9,39 @@
  
  <?php
       session_start();
+      $servername = getenv('IP');
+      $username = getenv('C9_USER');
+      $password = "";
+      $database = "ode_food_poll";
+      $dbport = 3306;
+      
+      
+    $conn = new mysqli($servername, $username, $password, $database, $dbport);
+    
+    
       echo $_SESSION['click_id'];
       echo "<br />";
       echo $_POST["votevalue"];
+      
+      $click_id=$_SESSION['click_id'];
+      
+      $actual_ct_sql = "SELECT actual_serving_count FROM food_list WHERE id=".$click_id;
+      
+      $actual_ct_result=mysqli_query($conn, $actual_ct_sql);
+      
+      $actual_ct_row=mysqli_fetch_array($actual_ct_result,MYSQLI_NUM);
+      
+      $actual_ct_data= $actual_ct_row[0];
+      
+      echo $actual_ct_data;
+      
+      
+        // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    
+  
+      $conn->close();
  
  ?>
