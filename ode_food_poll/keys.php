0d6e1916f6c6fa4056167881814e0520e6f34808
odekyc <odekyc@gmail.com>
2018-04-18 09:40:58 -0700

    100% deployed to heroku version fully repaired

--- ode_food_poll/keys.php
+++ ode_food_poll/keys.php
@@ -1,10 +1,10 @@
 <?php
  
-   $consumerKey1='UfWWMY3MQb208gLHmFjfoWbal';
-   $consumerSecret1='bnRULSBeVQN1zcYdR8oQZ58k6R5IM5uX4jkeJVCF20eGu6uHHT';
+   $consumerKey1='MbXni0FJ3BF1huyii8mNYXK3A';
+   $consumerSecret1='M8w1GWryu7EvFpLvEoiN8BWIpc1QUwlaoe9XcX4KHcSQtftz3i';
    
-   $consumerKey2='zsUetlaJUhozl1pZcKaVnRNew';
-   $consumerSecret2='ZrCQYLMeiS5vmadGcLSi2OA7XfVPfwMfUH5hGF2Qwty4w45vzT';
+   $consumerKey2='rxjUk95tq62i2kFcOcnflpgVr';
+   $consumerSecret2='ResUDC55QJuScEhOHSSrIWDQc1FBYwKcw5lLWkriV0TnFfjJa2';
    
    
    
